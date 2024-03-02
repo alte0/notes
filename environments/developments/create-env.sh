e23ce97997c95f0f509bebe67abc2b51006f1903
Максим Дмитриев <makc-dmv@yandex.ru>
2024-03-02 15:39:51 +0500

    setting docker files

--- environments/developments/create-env.sh
+++ environments/developments/create-env.sh
@@ -6,6 +6,9 @@ if [ -f "$file" ]; then
 else
     touch "$file"
     echo 'PROJECT_NAME=notes' >> "$file"
+    echo '#php-fpm' >> "$file"
     echo 'USER_ID='$(id -u) >> "$file"
     echo 'GROUP_ID='$(id -g) >> "$file"
+    echo '#db' >> "$file"
+    echo 'PASSWORD_DB=''' >> "$file"
 fi
