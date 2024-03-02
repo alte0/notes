53468654fb8d561fc2120ebe861b9c956ebc584d
Максим Дмитриев <makc-dmv@yandex.ru>
2024-03-02 11:48:42 +0500

    adding docker

---
+++ environments/developments/create-env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+file=$(dirname $(realpath "$0"))/.env
+if [ -f "$file" ]; then
+    echo ".env already exist."
+else
+    touch "$file"
+    echo 'PROJECT_NAME=notes' >> "$file"
+    echo 'USER_ID='$(id -u) >> "$file"
+    echo 'GROUP_ID='$(id -g) >> "$file"
+fi
